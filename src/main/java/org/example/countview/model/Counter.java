eca3039b0e308bf16b8ad47b54a9e07249742a9d
hainam1411 <hainam141199@gmail.com>
2024-06-25 09:19:43 +0700

    commit 1

---
+++ src/main/java/org/example/countview/model/Counter.java
@@ -0,0 +1,23 @@
+package org.example.countview.model;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public void increment() {
+        this.count++;
+    }
+}
